0820c6d0bff2871d4a52091a1bff55e0d56f3934
Antossio <antossio@gmail.com>
2015-02-15 22:16:10 +0200

    Initial commit

---
+++ src/Button.java
@@ -0,0 +1,24 @@
+import java.awt.Color;
+import java.io.Serializable;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+
+	public class Button extends JFrame{
+		public int index; //индекс в массиве клеток
+		public boolean open;  //открыта ли €чейка
+		public JButton but=new JButton();
+		public JLabel lab=new JLabel ();
+
+		public Button(int a, int b, int c, int d){
+			but.setBounds(a,b,c,d);
+			lab.setBounds(a,b,c,d);
+			lab.setBorder(new LineBorder(Color.LIGHT_GRAY, 1));
+			lab.setHorizontalAlignment(SwingConstants.CENTER);
+			lab.setVerticalAlignment(SwingConstants.CENTER);
+
+		}
+}
